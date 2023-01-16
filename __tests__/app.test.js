8ff7b36a03dc3c46e63ed46047bd74d8dd297093
Darren80 <we444465@gmail.com>
2023-01-16 16:30:35 +0000

    task 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -22,17 +22,29 @@ describe('GET app.js', () => {
         });
     });
 
-    // test('GET /api/articles', () => {
-    //     return request(app)
-    //     .get('/api/articles')
-    //     .expect(200)
-    //     .then((response) => {
-    //         // console.log(response, "<-- response");
-    //         expect(response).toHaveProperty("rows");
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //     });;
-    // });
+    test('GET /api/articles', () => {
+        return request(app)
+        .get('/api/articles')
+        .expect(200)
+        .then((response) => {
+            response = response.body;
+            console.log(response[0]);
+            expect(response[0]).toEqual(
+                expect.objectContaining({
+                article_id: expect.any(Number),
+                title: expect.any(String),
+                topic: expect.any(String),
+                author: expect.any(String),
+                body: expect.any(String),
+                created_at: expect.any(String),
+                votes: expect.any(Number),
+                article_img_url: expect.any(String)
+            })
+            )
+        })
+        .catch(err => {
+            console.log(err);
+        });;
+    });
 })
 });
